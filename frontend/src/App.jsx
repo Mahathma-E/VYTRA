4e50d9c21fcba9a89f8e29bb5a69679599689c2b
Mahathma-E <mjet3517@gmail.com>
2025-10-13 20:51:02 +0530

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -83,7 +83,7 @@ function App() {
             } 
           />
           <Route 
-            path="/alerts" 
+            path="/alert" 
             element={
               <ProtectedRoute>
                 <AlertsList />
